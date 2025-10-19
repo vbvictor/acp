14b2e5e2046b69fc8dd3426b73c039fd593ca3ca
Victor Baranov <bar.victor.2002@gmail.com>
2025-10-19 20:10:28 +0300

    feat: add python formatter

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""A simple Python script."""
+
+def main() -> None:
+    """Main entry point"""
+    message =    "Hello world"
+    print(message)
+
+
+if __name__ == "__main__":
+    main()
