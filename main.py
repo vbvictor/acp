b7d2af5cf3f7bca75f097c70d1a2470cd97127bf
Victor Baranov <bar.victor.2002@gmail.com>
2025-10-19 20:12:25 +0300

    fix formatting

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """A simple Python script."""
 
+
 def main() -> None:
     """Main entry point"""
-    message =    "Hello world"
+    message = "Hello world"
     print(message)
 
 
